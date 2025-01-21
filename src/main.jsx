4294d06f113fb0dbb01e3a8f5babc9538def0bcb
ToriIurina <tori.iurina@yandex.ru>
2025-01-21 19:15:43 +0300

    Fix according to comments

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,22 @@ const reactRoot = createRoot(rootElement);
 
 reactRoot.render(
   <ul>
-    {restaurants.map((item, index) => (
-      <li key={item + index}>
+    {restaurants.map((item) => (
+      <li key={item.id}>
         <h2>{item.name}</h2>
         <div>
           <h3>Меню</h3>
           <ul>
-            {item.menu.map((itemMenu, index) => (
-              <li key={itemMenu + index}>{itemMenu.name}</li>
+            {item.menu.map((itemMenu) => (
+              <li key={itemMenu.id}>{itemMenu.name}</li>
             ))}
           </ul>
         </div>
         <div>
           <h3>Отзывы</h3>
           <ul>
-            {item.reviews.map((itemReviews, index) => (
-              <li key={itemReviews + index}>{itemReviews.text}</li>
+            {item.reviews.map((itemReviews) => (
+              <li key={itemReviews.id}>{itemReviews.text}</li>
             ))}
           </ul>
         </div>
