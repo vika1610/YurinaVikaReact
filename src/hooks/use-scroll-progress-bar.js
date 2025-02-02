ccc1d421e7a8c941227065639f67a8e1e12b5786
ToriIurina <tori.iurina@yandex.ru>
2025-02-02 18:38:56 +0300

    Fix according to comments

--- src/hooks/use-scroll-progress-bar.js
+++ src/hooks/use-scroll-progress-bar.js
@@ -3,25 +3,32 @@ import { useEffect, useState } from 'react';
 export const useScrollProgressBar = () => {
   const [progress, setProgress] = useState(0);
 
+  const getProgress = () => {
+    const scrollHeight = document.body.scrollHeight;
+    const visibleHeight = window.innerHeight;
+
+    const restHeight = scrollHeight - visibleHeight;
+    const scrollPosition = window.scrollY;
+    let progress = 0;
+
+    if (restHeight > 0) {
+      progress = (scrollPosition / restHeight) * 100;
+    }
+
+    return progress;
+  };
+
   useEffect(() => {
+    const updateProgressBar = () => {
+      setProgress(getProgress());
+    };
+
     window.addEventListener('scroll', updateProgressBar);
 
     return () => {
       window.removeEventListener('scroll', updateProgressBar);
     };
   }, []);
-
-  const updateProgressBar = () => {
-    const scrollHeight = document.body.scrollHeight;
-    const visibleHeight = window.innerHeight;
-
-    const restHeight = scrollHeight - visibleHeight;
-    const scrollPosition = window.scrollY;
-
-    const progress = (scrollPosition / restHeight) * 100;
-
-    setProgress(progress);
-  };
 
   return progress;
 };
